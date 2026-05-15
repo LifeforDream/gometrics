cee0eff76c34a4248b7fe939d28b7ee15e52a4e1
MZhitkov <lifefordream2010@mail.ru>
2026-05-15 16:00:24 +0300

    Fix unsafe storage access
    - also delete GlobalStorage

--- internal/repository/memrepo.go
+++ internal/repository/memrepo.go
@@ -6,23 +6,14 @@ type MemStorage struct {
 	store map[string]models.Metrics
 }
 
-var GlobalStorage = MemStorage{
-	store: make(map[string]models.Metrics),
-}
-
 func (m *MemStorage) GetMetric(name string) (models.Metrics, bool) {
 	metric, ok := m.store[name]
 	return metric, ok
 }
 
 func (m *MemStorage) SetMetric(metricVal models.Metrics) {
+	if m.store == nil {
+		m.store = make(map[string]models.Metrics)
+	}
 	m.store[metricVal.ID] = metricVal
-}
-
-func GetMetric(name string) (models.Metrics, bool) {
-	return GlobalStorage.GetMetric(name)
-}
-
-func SetMetric(metricVal models.Metrics) {
-	GlobalStorage.SetMetric(metricVal)
 }
